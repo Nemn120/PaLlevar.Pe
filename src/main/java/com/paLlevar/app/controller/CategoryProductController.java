0dd9415e0ac8ac42e84251032e564409f46c7019
Jhonatan <Jhonatan@192.168.1.8>
2020-06-24 00:52:52 -0500

    commit 1.3

--- src/main/java/com/paLlevar/app/controller/CategoryProductController.java
+++ src/main/java/com/paLlevar/app/controller/CategoryProductController.java
@@ -1,5 +1,39 @@
 package com.paLlevar.app.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.paLlevar.app.model.entities.CategoryProductEntity;
+import com.paLlevar.app.model.services.CategoryProductService;
 
+@RestController
+@RequestMapping("/categoryproduct")
 public class CategoryProductController {
-
+	String path = "http://localhost:8080/categoryproduct";
+	
+	@Autowired
+	private CategoryProductService categoryProductService;
+	
+	
+	@GetMapping(path="/glcp")
+	public ResponseEntity<List<CategoryProductEntity>>  getListCategoryProduct(){
+		List<CategoryProductEntity>  lista=categoryProductService.getAll();
+		return new ResponseEntity<List<CategoryProductEntity>>(lista,HttpStatus.OK);
+	}
+	@PostMapping(path="/scp")
+	public CategoryProductEntity saveCategoryProduct(@RequestBody CategoryProductEntity cpe){
+		CategoryProductEntity categoryProductEntity = categoryProductService.save(cpe);
+		return categoryProductEntity;
+	}
+	@DeleteMapping(value="/dcp/{id}")
+	public void deletedCategoryProduct(@PathVariable("id")Integer id) {
+		categoryProductService.deleteById(id);
+	}
 }
--- src/main/java/com/paLlevar/app/controller/ClientController.java
+++ src/main/java/com/paLlevar/app/controller/ClientController.java
@@ -1,5 +1,37 @@
 package com.paLlevar.app.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.paLlevar.app.model.entities.ClientEntity;
+import com.paLlevar.app.model.services.ClientService;
 
+@RestController
+@RequestMapping("/client")
 public class ClientController {
 
+	@Autowired
+	private ClientService clientService;
+	
+	@GetMapping(path="/glcl")
+	public ResponseEntity<List<ClientEntity>>  getListClient(){
+		List<ClientEntity>  lista=clientService.getAll();
+		return new ResponseEntity<List<ClientEntity>>(lista,HttpStatus.OK);
+	}
+	@PostMapping(path="/scl")
+	public ClientEntity saveClient(@RequestBody ClientEntity cle){
+		ClientEntity categoryProductEntity = clientService.save(cle);
+		return categoryProductEntity;
+	}
+	@DeleteMapping(value="/dcl/{id}")
+	public void deletedClient(@PathVariable("id")Integer id) {
+		clientService.deleteById(id);
+	}
 }
